ef64e7c444699143b03c3928c3df42879580bd4d
SAWADA Tadashi <cesare@mayverse.jp>
2020-11-21 22:07:23 +0900

    Refactor method for waiting input

--- src/echo.rs
+++ src/echo.rs
@@ -1,12 +1,11 @@
 use std::io::{self};
 
-fn echo() -> Option<usize> {
+fn wait_for_input() -> Option<String> {
     let mut buffer = String::new();
     match io::stdin().read_line(&mut buffer) {
         Ok(0) => None,
-        Ok(n) => {
-            print!("{}", buffer);
-            Some(n)
+        Ok(_) => {
+            Some(buffer)
         }
         Err(_) => None
     }
@@ -14,8 +13,8 @@ fn echo() -> Option<usize> {
 
 fn main() {
     loop {
-        match echo() {
-            Some(_) => {}
+        match wait_for_input() {
+            Some(text) => { print!("{}", text) }
             None => { break; }
         }
     }
