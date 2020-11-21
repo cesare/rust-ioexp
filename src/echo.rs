4a6e729b954a10020609b13d5cfd01b19ee7e720
SAWADA Tadashi <cesare@mayverse.jp>
2020-11-21 21:37:09 +0900

    Read one line only

--- src/echo.rs
+++ src/echo.rs
@@ -1,8 +1,8 @@
-use std::io::{self, Read};
+use std::io::{self};
 
 fn main() -> io::Result<()> {
-  let mut buffer = String::new();
-  io::stdin().read_to_string(&mut buffer)?;
-  println!("{}", buffer);
-  Ok(())
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    println!("{}", buffer);
+    Ok(())
 }
