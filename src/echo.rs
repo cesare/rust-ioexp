696acb93d4f268e7cb45f16490dc9fb9a2bb592a
SAWADA Tadashi <cesare@mayverse.jp>
2020-11-21 21:56:28 +0900

    Loop through all lines

--- src/echo.rs
+++ src/echo.rs
@@ -1,8 +1,22 @@
 use std::io::{self};
 
-fn main() -> io::Result<()> {
+fn echo() -> Option<usize> {
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    println!("{}", buffer);
-    Ok(())
+    match io::stdin().read_line(&mut buffer) {
+        Ok(0) => None,
+        Ok(n) => {
+            print!("{}", buffer);
+            Some(n)
+        }
+        Err(_) => None
+    }
+}
+
+fn main() {
+    loop {
+        match echo() {
+            Some(_) => {}
+            None => { break; }
+        }
+    }
 }
