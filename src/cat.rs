0f8b3e47a0e5cf698a81cbfb13eb6248ab49d5bf
SAWADA Tadashi <cesare@mayverse.jp>
2020-11-25 00:16:29 +0900

    Enable binary read/write

--- src/cat.rs
+++ src/cat.rs
@@ -1,16 +1,22 @@
 use std::env::args;
-use std::fs::{File};
-use std::io::{self, BufRead, BufReader};
+use std::fs::File;
+use std::io::{self, BufReader, Read, Write};
 use std::path::Path;
 
 fn show(path_name: &str) -> Result<(), io::Error> {
     let path = Path::new(path_name);
     let file = File::open(path)?;
-    let f = BufReader::new(file);
+    let mut f = BufReader::new(file);
 
-    for result in f.lines() {
-        let line = result?;
-        println!("{}", line);
+    let mut buf = [0; 1024];
+    loop {
+        match f.read(&mut buf)? {
+            0 => break,
+            n => {
+                let content = &buf[..n];
+                io::stdout().write(content)?;
+            }
+        }
     }
 
     Ok(())
