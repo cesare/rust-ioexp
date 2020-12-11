84360c79f1a170ae638c9b80dd591d43f4ac7941
SAWADA Tadashi <cesare@mayverse.jp>
2020-12-11 21:00:08 +0900

    Use stream for async_cat

--- src/async_cat.rs
+++ src/async_cat.rs
@@ -1,16 +1,44 @@
 use std::env::args;
+use std::pin::Pin;
 use async_std::fs::File;
-use async_std::io::{self, BufReader};
+use async_std::io::{self};
 use async_std::prelude::*;
+use async_std::task::{Context, Poll};
 use std::vec::Vec;
+use pin_project::pin_project;
+
+#[pin_project]
+struct FileInputStream {
+    file: File,
+}
+
+impl FileInputStream {
+    fn new(file: File) -> FileInputStream {
+        FileInputStream { file: file }
+    }
+}
+
+impl Stream for FileInputStream {
+    type Item = Result<Vec<u8>, io::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let mut buf = [0; 1024];
+        let mut f = self.project().file.read(&mut buf);
+        match Pin::new(&mut f).poll(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(Ok(0))  => { Poll::Ready(None) },
+            Poll::Ready(Ok(n))  => { Poll::Ready(Some(Ok(buf[..n].to_vec()))) },
+            Poll::Ready(Err(e)) => { Poll::Ready(Some(Err(e)))},
+        }
+    }
+}
 
 async fn show(path: &str) -> Result<(), io::Error> {
     let file = File::open(path).await?;
-    let reader = BufReader::new(file);
-    let mut lines = reader.lines();
+    let mut stream = FileInputStream::new(file);
 
-    while let Some(line) = lines.next().await {
-        println!("{}", line?);
+    while let Some(content) = stream.next().await {
+        io::stdout().write(&content?).await?;
     }
 
     Ok(())
