5b18feaf75824a77c7cecc078c9f09c07983840c
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-11 20:54:35 +0900

    Enable to specify ip address and port with arguments

--- src/echo_server.rs
+++ src/echo_server.rs
@@ -1,10 +1,25 @@
 use async_std::io::{self};
 use async_std::net::TcpListener;
 use async_std::prelude::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "echo_server")]
+struct Opt {
+    #[structopt(short, long, default_value = "127.0.0.1")]
+    bind: String,
+
+    #[structopt(short, long, default_value = "8080")]
+    port: u32,
+}
 
 #[async_std::main]
 async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let opt = Opt::from_args();
+    let bind_address = format!("{}:{}", opt.bind, opt.port);
+    println!("Waiting for requests on {}", bind_address);
+
+    let listener = TcpListener::bind(bind_address).await?;
     let mut incoming = listener.incoming();
 
     while let Some(stream) = incoming.next().await {
