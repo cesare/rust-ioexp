860b544ae1e5ba4146da43438b4ad9d2ae62f604
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-12 22:42:53 +0900

    Enable client to specify ip address and port with arguments

--- src/echo_client.rs
+++ src/echo_client.rs
@@ -1,6 +1,17 @@
 use async_std::io::{self};
 use async_std::net::TcpStream;
 use async_std::prelude::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "echo_server")]
+struct Opt {
+    #[structopt(short, long, default_value = "127.0.0.1")]
+    bind: String,
+
+    #[structopt(short, long, default_value = "8080")]
+    port: u32,
+}
 
 async fn read_line() -> io::Result<Option<String>> {
     let mut buf = String::new();
@@ -12,7 +23,9 @@ async fn read_line() -> io::Result<Option<String>> {
 
 #[async_std::main]
 async fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
+    let opt = Opt::from_args();
+    let bind_address = format!("{}:{}", opt.bind, opt.port);
+    let mut stream = TcpStream::connect(bind_address).await?;
 
     while let Some(message) = read_line().await? {
         stream.write_all(message.as_bytes()).await?;
