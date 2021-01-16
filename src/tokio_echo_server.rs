ab4ffb89e0600a2316358b13de6f03f4d0e480db
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-16 22:43:49 +0900

    Add tokio_echo_server

---
+++ src/tokio_echo_server.rs
@@ -0,0 +1,37 @@
+use tokio::io::{self};
+use tokio::net::{TcpListener, TcpStream};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "echo_server")]
+struct Opt {
+    #[structopt(short, long, default_value = "127.0.0.1")]
+    bind: String,
+
+    #[structopt(short, long, default_value = "8080")]
+    port: u32,
+}
+
+impl Opt {
+    fn bind_address(&self) -> String {
+        format!("{}:{}", self.bind, self.port)
+    }
+}
+
+async fn handle(stream: TcpStream) {
+    let (mut reader, mut writer) = stream.into_split();
+    let _ = io::copy(&mut reader, &mut writer).await;
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let opt = Opt::from_args();
+    let bind_address = opt.bind_address();
+    println!("Waiting for requests on {}", bind_address);
+
+    let listener = TcpListener::bind(bind_address).await?;
+    loop {
+        let (tcpstream, _) = listener.accept().await?;
+        handle(tcpstream).await;
+    }
+}
