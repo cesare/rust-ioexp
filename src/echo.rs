e2fd6790536d6e84dc5577034b4181f4b64c0c5e
SAWADA Tadashi <cesare@mayverse.jp>
2022-05-02 20:12:55 +0900

    Use map

--- src/echo.rs
+++ src/echo.rs
@@ -3,7 +3,7 @@ use std::io::{self};
 fn wait_for_input() -> Result<Option<String>, io::Error> {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer)
-        .and_then(|n| if n == 0 { Ok(None) } else { Ok(Some(buffer)) })
+        .map(|n| if n == 0 { None } else { Some(buffer) })
 }
 
 fn main() -> Result<(), io::Error> {
